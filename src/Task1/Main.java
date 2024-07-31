c31db7bd4b824eb68145493fa8f03e3898cf2e63
ярослав <yaracrut245@gmail.com>
2024-07-31 13:27:26 +0500

    1

--- src/Task1/Main.java
+++ src/Task1/Main.java
@@ -1,2 +1,22 @@
-package Task1;public class Main {
+package Task1;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer,String> towns = new HashMap<>();
+        towns.put(1, "Москва");
+        towns.put(2, "Самара");
+        towns.put(3, "Тюмень");
+        towns.put(4, "Екатеринбург");
+        System.out.println(keyHashMapTowns(towns));
+
+    }
+    public static Collection<Integer> keyHashMapTowns(HashMap<Integer, String> mapExamp){
+        Collection<Integer> key = mapExamp.keySet();
+        return key;
+    }
 }
--- src/Task2/Main.java
+++ src/Task2/Main.java
@@ -1,2 +1,29 @@
-package Task2;public class Main {
+package Task2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer, String> cars = new HashMap<>();
+        cars.put(1, "Жигули");
+        cars.put(2, "ВАЗ");
+        cars.put(3, "Пятнашка");
+        cars.put(4, "Запорожец");
+        System.out.println(sortCars(cars));
+    }
+
+
+    public static Map<Integer,String>   sortCars(Map<Integer, String> mapExamp) {
+        Map< Integer,String> mapCar = new HashMap<>();
+        for (Map.Entry <Integer,String> sortCar : mapExamp.entrySet()) {
+            if (sortCar.getValue().toString().length() > 3){
+                mapCar.put(sortCar.getKey(), sortCar.getValue());
+
+            }
+        }
+        return mapCar;
+    }
 }
